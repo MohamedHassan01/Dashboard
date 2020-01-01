33aa77dee56ec307f66504e6c0b481c9c8d3be08
Mohamed <mohamed.pe20188@gmail.com>
2020-01-01 14:35:54 +0200

    Add sidebar and content-page

---
+++ src/components/sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sidebar">
+    Sidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="page home-page">
+    <Sidebar />
+
+    <div class="content-area">
+      <h1>Home</h1>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Sidebar from "@/components/sidebar";
 
 export default {
   name: "home",
+
   components: {
-    HelloWorld
+    Sidebar
   }
 };
 </script>
